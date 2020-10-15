af058d75c31d0426360983dc193652969163a42e
xpewa <xpewa10@gmail.com>
2020-10-15 10:33:51 +0300

    change .travis.yml

--- IZ-1/CI/Travis/install.sh
+++ IZ-1/CI/Travis/install.sh
@@ -1,28 +1,12 @@
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update -qq
+sudo pip install codecov
 
-sudo apt-get install -qq g++-5
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
+sudo apt-get install -qq cppcheck
 
-CMAKE_VERSION=3.3.2
-CMAKE_VERSION_DIR=v3.3
+sudo apt-get install valgrind
 
-CMAKE_OS=Linux-x86_64
-CMAKE_TAR=cmake-$CMAKE_VERSION-$CMAKE_OS.tar.gz
-CMAKE_URL=http://www.cmake.org/files/$CMAKE_VERSION_DIR/$CMAKE_TAR
-CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
+sudo apt-get install cmake
 
-wget --quiet $CMAKE_URL
-mkdir -p $CMAKE_DIR
-tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
-export PATH=$CMAKE_DIR/bin:$PATH
+sudo wget https://github.com/google/googletest/archive/release-1.7.0.tar.gz
 
-if [ "$TARGET_CPU" == "x86" ]; then
-    sudo dpkg --add-architecture i386
-    sudo apt-get -qq update
+sudo cmake -DBUILD_SHARED_LIBS=ON
 
-    sudo apt-get install -y liblua5.2-dev:i386
-    sudo apt-get install -y libusb-1.0:i386
-
-    sudo apt-get install -y g++-5-multilib
-fi
